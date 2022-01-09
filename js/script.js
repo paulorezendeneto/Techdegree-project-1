e09b1001286d6374590f8c352169c9a3ca7e9e6d
paulorezendeneto <paulorezendeneto@gmail.com>
2022-01-09 02:00:33 +0000

    commit it

--- js/script.js
+++ js/script.js
@@ -3,13 +3,59 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
+// For assistance:
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
+/***
+ * `quotes` array
 ***/
+
+const quotes = [
+  {
+    quote: 'The greatest glory in living lies not in never falling, but in rising every time we fall.',
+    source: 'Nelson Mandela',
+    citation: '',
+    year: '',
+  },
+  {
+    quote: 'Spread love everywhere you go. Let no one ever come to you without leaving happier.',
+    source: 'Mother Teresa',
+    citation: '',
+    year: '',
+  },
+  {
+    quote: 'The only impossible journey is the one you never begin.',
+    source: 'Tony Robbins',
+    citation: '',
+    year: '',
+  },
+  {
+    quote: 'Two things are infinite: the universe and human stupidity; and I am not sure about the universe.',
+    source: 'Albert Einstein',
+    citation: '',
+    year: '',
+  },
+  {
+    quote: 'The Force will be with you. Always.',
+    source: 'Obi-Wan Kenobi',
+    citation: 'Star Wars',
+    year: '',
+  },
+  {
+    quote: 'That which does not kill us makes us stronger.',
+    source: 'Friedrich Nietzsche',
+    citation: '',
+    year: '1888',
+  },
+  {
+    quote: 'You only live once, but if you do it right, once is enough.',
+    source: 'Mae West',
+    citation: '',
+    year: '',
+  },
+]
+
 
 
 
